f577f8f46694bb79b4677da13c10f76e7d11fe68
Rangi <remy.oukaour+rangi42@gmail.com>
2020-06-02 18:20:41 -0400

    Version 3.2.0

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define PROGRAM_VERSION 3,1,0
+#define PROGRAM_VERSION 3,2,0
 #ifdef _DEBUG
-#define PROGRAM_VERSION_STRING "3.1.0 [DEBUG]"
+#define PROGRAM_VERSION_STRING "3.2.0 [DEBUG]"
 #else
-#define PROGRAM_VERSION_STRING "3.1.0"
+#define PROGRAM_VERSION_STRING "3.2.0"
 #endif
 
 #define PROGRAM_EXE_NAME "tilemapstudio"
@@ -20,6 +20,6 @@
 
 #define PROGRAM_AUTHOR "Rangi"
 
-#define CURRENT_YEAR "2019"
+#define CURRENT_YEAR "2020"
 
 #endif
